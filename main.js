0245c165f5e0c49e20c0c9054bbc2ebda5b5b905
Sarah Elhwarii <56038677+sarahelhwarii@users.noreply.github.com>
2020-07-08 17:59:04 +0300

    error mod

--- main.js
+++ main.js
@@ -14,17 +14,66 @@ button.addEventListener("click", () => {
 //getting ingredients and mesaures
 
 const createMeal = (meal) => {
-  let ingerdients = [];
+  let ingredients = [];
   //for up to 20 ingredients
-  for (let i = 0; i < 20; i++) {
+  for (let i = 1; i < 20; i++) {
     if (meal[`strIngredient1${i}`]) {
-      ingerdients.push(
-        `${meal[`strIngredient${i}`]}-${meal[`strIngredient${i}`]}`
+      ingredients.push(
+        `${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`
       );
     } else {
       break;
     }
   }
-};
 
-//*********DISPLAYING RECIPE INFO */
+  //*********DISPLAYING RECIPE INFO */
+  const newInnerHTML = `
+    <div class="row">
+        <div class="columns five >
+            <img src="${meal.strMealThumb}" alt="Meal Image">
+
+            ${
+              meal.strArea
+                ? `<p><strong>Category:</strong>${meal.strArea}</p>`
+                : ""
+            }
+            ${
+              meal.strTags
+                ? `<p><strong>Category:</strong>${meal.strTags
+                    .split(",")
+                    .join(",")}</p>`
+                : ""
+            }
+            <h5>Ingredients</h5>
+            <ul>
+            ${ingredients
+              .map((ingredient) => `<li>${ingredient}</li>`)
+              .join("")}
+            </ul>
+        </div>
+        <div class="columns seven>
+            <h4>${meal.strMeal}</h4>
+            <p>${meal.strInstructions}</p>
+        </div>
+    </div>
+    ${
+      meal.strYoutube
+        ? `
+    <div class="row">
+            <h5>Video Recipe</h5>
+            <div class="videoWrapper">
+                <iframe width="420 height="315"
+                src="https://www.youtube.com/embed/${meal.strYoutube.slice(
+                  -11
+                )}">
+                </iframe>
+
+            </div>
+
+    </div>`
+        : ""
+    }
+
+    `;
+  meal_area.innerHTML = newInnerHTML;
+};
