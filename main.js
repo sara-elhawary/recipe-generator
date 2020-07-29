d76fe7d5a3af983ac3d306af32f06aa780a15b40
Sarah Elhwarii <56038677+sarahelhwarii@users.noreply.github.com>
2020-07-29 07:23:12 +0300

    add js

--- main.js
+++ main.js
@@ -1,4 +1,66 @@
-const mealRow = document.getElementsById("meal");
-const btn = document.getElementsByClassName("button-primary");
+const mealRow = document.getElementById("meal");
+const btn = document.getElementById("get_meal");
 
-btn.addEventListener("click", () => {});
+btn.addEventListener("click", () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((res) => res.json())
+    .then((res) => {
+      createMeal(res.meals[0]);
+      console.log(res);
+    });
+});
+
+const createMeal = (meal) => {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  const newInnerHTML = `
+  <div class="row">
+    <div class="columns five  ">
+      <img src="${meal.strMealThumb}" alt="Meal Image">
+      ${
+        meal.strCategory
+          ? `<p><strong>Category:</strong>${meal.strCategory}</p>`
+          : ""
+      }
+      ${meal.strArea ? `<p><strong>Area:</strong>${meal.strArea}</p>` : ""}
+      ${
+        meal.strTags
+          ? `<p><strong>Tags:</strong>${meal.strTags.split(",").join(",")}</p>`
+          : ""
+      }
+      <h5>Ingredients</h5>
+      <ul>
+        ${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+      </ul>
+    </div>
+    <div class="columns seven  ">
+      <h4>${meal.strMeal}</h4>
+      <p>${meal.strInstructions}</p>
+    </div>
+</div>
+${
+  meal.strYoutube
+    ? `
+  <div class="row  ">
+      <h5>Video Recipe</h5>
+      <div class="videoWrapper  ">
+        <iframe width="420" height="315"
+        src="http://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+        </iframe>
+      </div>
+  </div>
+`
+    : ""
+}
+  `;
+  mealRow.innerHTML = newInnerHTML;
+};
